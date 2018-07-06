a293991a5d7ddaa4e9dc1f43aa4f91e4cd1a0969
ioan.rabagel <ioan.rabagel@visma.com>
2018-07-06 14:38:16 +0300

    new java project

---
+++ app/src/main/java/com/rabagel/marian/guardcheckpoint/Login/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.rabagel.marian.guardcheckpoint.Login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.rabagel.marian.guardcheckpoint.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
